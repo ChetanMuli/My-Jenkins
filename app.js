529635fcf17cf34ed8d63bcb1e414911fea99999
Chetan Muli <chetan.muli4321@gmail.com>
2026-02-03 21:48:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Checking Auto-Deploy 🚀");
+  res.send("Checking 🚀");
 });
 
 app.listen(3000, () => {
